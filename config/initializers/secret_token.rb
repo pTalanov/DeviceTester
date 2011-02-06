d0206dd07a3d0640149c1807bb8e9dda6b58c9ad
Talanov Pavel <talanov.pavel@gmail.com>
2011-02-06 11:12:42 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviceTester::Application.config.secret_token = '437cabfe30f700cae4f3df782896c185e21496839cfb2a7910aa65e4c9105bb891f2006c47e6b6c4cee5f6a94cedd8adeb6aa02ebc5110d21a19f8dc2604f85d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DeviceTester::Application.config.session_store :cookie_store, :key => '_DeviceTester_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DeviceTester::Application.config.session_store :active_record_store
